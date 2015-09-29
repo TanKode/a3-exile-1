18bf862eb5fdb2ec11f34358998a57be78e7c0c4
Tom Witkowski <dev.gummibeer@gmail.com>
2015-09-29 12:26:33 +0200

    root files

---
+++ start-restart-server.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: KILL BATTLEYE IF IT ISN'T ALREADY
+taskkill /im Bec.exe
+
+echo Stopping the server for restart
+echo Stopping arma3server2.exe ...
+taskkill /f /im arma3server2.exe
+echo Server is stopped now ... restarting
+timeout /t 10
+echo Starting Server
+cd C:\Arma3Server\Arma3Exile
+start "arma3" /min /high "arma3server2.exe" "-profiles=C:\Arma3Server\Arma3Exile\config" "-BEPath=C:\Arma3Server\Arma3Exile\battleye" "-config=C:\Arma3Server\Arma3Exile\config\config.cfg" "-cfg=C:\Arma3Server\Arma3Exile\config\basic.cfg" -port=2316 -world=empty -autoinit -high -loadMissionToMemory -noSound -malloc=system -nosplash -cpuCount=4 -maxmem=3047 "-mod=@extDB2;@RyL_server;@asm"
+
+::RESTARTING BATTLEYE
+set becpath="C:\Arma3Server\Arma3Exile\bec"
+cd /d %becpath%
+start "" /min "bec2.exe" -f config.cfg
+timeout 3
+echo Battleye has started.. 
