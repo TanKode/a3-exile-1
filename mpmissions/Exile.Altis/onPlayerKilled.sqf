4b16d4f54084209712835795aef51943d02c3867
Git Bambusfarm <git@bambusfarm.net>
2015-10-04 19:03:07 +0200

    bugfixes and small changes

--- mpmissions/Exile.Altis/onPlayerKilled.sqf
+++ mpmissions/Exile.Altis/onPlayerKilled.sqf
@@ -7,7 +7,7 @@ _curRespect = _victim getVariable ["ExileScore", 0];
 _remRespect = _curRespect * 0.05;
 if(_remRespect < 100) {
     _remRespect = 100;
-}
+};
 _newRespect = _curRespect - _remRespect;
 _victim setVariable ["ExileScore", _newRespect];
 ExileClientPlayerScore = _newRespect;
--- start-restart-server.bat
+++ start-restart-server.bat
@@ -11,3 +11,10 @@ timeout /t 10
 echo Starting Server
 cd C:\Arma3Server\Arma3Exile
 start "arma3" /min /high "arma3server2.exe" "-profiles=C:\Arma3Server\Arma3Exile\config" "-BEPath=C:\Arma3Server\Arma3Exile\battleye" "-config=C:\Arma3Server\Arma3Exile\config\config.cfg" "-cfg=C:\Arma3Server\Arma3Exile\config\basic.cfg" -port=2312 -world=empty -autoinit -high -loadMissionToMemory -noSound -malloc=system -nosplash -cpuCount=4 -maxmem=3047 "-mod=@asm;@Exile;@ExileServer;@infiSTAR_Exile"
+
+::RESTARTING BATTLEYE
+set becpath="C:\Arma3Server\bec"
+cd /d %becpath%
+start "" /min "bec.exe" -f exile.cfg
+timeout 3
+echo Battleye has started.. 
